8047639a635d67bbadae1b7852e517587a55769b
Ramy Kaddouri <myramkadd@gmail.com>
2024-05-15 20:50:40 -0400

    basic movement done

--- src/main.c
+++ src/main.c
@@ -17,14 +17,19 @@ typedef struct {
 } Ship;
 
 void draw_ship(Ship *ship) {
-  Rectangle rect = {.x = ship->position.x,
+  Rectangle dest = {.x = ship->position.x,
                     .y = ship->position.y,
                     .width = ship->texture->width,
                     .height = ship->texture->height};
 
-  Vector2 origin = {.x = rect.width / 2, .y = rect.height / 2};
+  Rectangle src = {.x = 0,
+                   .y = 0,
+                   .width = ship->texture->width,
+                   .height = ship->texture->height};
 
-  DrawTexturePro(*ship->texture, rect, rect, origin, ship->rotation,
+  Vector2 origin = {.x = dest.width / 2, .y = dest.height / 2};
+
+  DrawTexturePro(*ship->texture, src, dest, origin, ship->rotation,
                  ship->color);
 }
 
@@ -46,20 +51,24 @@ int main(void) {
   while (!WindowShouldClose()) {
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
-    ship.rotation = (-180 / PI) * atan2(GetMouseY(), GetMouseX()) - 270;
+    ClearBackground(GetColor(0x111111));
+
+    Vector2 dist = Vector2Subtract(GetMousePosition(), ship.position);
+
+    ship.rotation = ((-180 / PI) * atan2(dist.y, -dist.x)) + 270;
+    ship.position.x = GetMouseX();
 
     draw_ship(&ship);
 
-    DrawLineV(ship.position, GetMousePosition(), RED);
-    DrawLineV(ship.position, (struct Vector2){GetMouseX(), ship.position.y},
-              RED);
-    DrawLineV(ship.position, (struct Vector2){ship.position.x, GetMouseY()},
-              RED);
+    // DrawLineV(ship.position, GetMousePosition(), RED);
+    // DrawLineV(ship.position, (struct Vector2){GetMouseX(), ship.position.y},
+    //           RED);
+    // DrawLineV((struct Vector2){GetMouseX(), ship.position.y},
+    //           (struct Vector2){GetMouseX(), GetMouseY()}, RED);
 
     snprintf(debug_text, 10 * sizeof(char), "%d FPS", GetFPS());
 
-    DrawText(debug_text, 10, 10, 22, PINK);
+    DrawText(debug_text, 10, 10, 32, PINK);
     EndDrawing();
   }
   free(debug_text);
